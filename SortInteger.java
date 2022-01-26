baf5e63e5696321b130fbf8233441a7653add089
Younusbaig <32571608+Younusbaig@users.noreply.github.com>
2022-01-26 19:38:56 +0500

    Update SortInteger.java

--- SortInteger.java
+++ SortInteger.java
@@ -1,6 +1,10 @@
 public class SortInteger {
 
     public static void sorted(int[] numArray) {
+
+        if (numArray == null)
+            throw new NullPointerException("array is null");
+
             for (int i = 0; i < numArray.length; i++) {
                 for (int j = i; j < numArray.length; j++) {
                     if (numArray[i] > numArray[j]) {
