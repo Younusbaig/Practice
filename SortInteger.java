7975b58ddb038879448543a3039662f1ccd3a8a1
Younusbaig <32571608+Younusbaig@users.noreply.github.com>
2022-01-26 19:31:13 +0500

    Update SortInteger.java

--- SortInteger.java
+++ SortInteger.java
@@ -1,19 +1,14 @@
 public class SortInteger {
-    
-    public static void sorted(int[] numArray) {
 
-        for (int i = 0; i < numArray.length; i++) {
-            for (int j = i; j < numArray.length; j++) {
-                if (numArray[i] > numArray[j]) {
-                    int temp = numArray[i];
-                    numArray[i] = numArray[j];
-                    numArray[j] = temp;
+    public static void sorted(int[] numArray) {
+            for (int i = 0; i < numArray.length; i++) {
+                for (int j = i; j < numArray.length; j++) {
+                    if (numArray[i] > numArray[j]) {
+                        int temp = numArray[i];
+                        numArray[i] = numArray[j];
+                        numArray[j] = temp;
+                    }
                 }
             }
         }
-        System.out.println("final values after sort");
-        for (int i = 0; i < numArray.length; i++) {
-            System.out.println(numArray[i] + ",");
-        }
     }
-}
